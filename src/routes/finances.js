b1a55d641f2ff87ca3f3d1bdc66b075706d9de1c
Nicolas Ferro <nicolasalexandrinoferro@gmail.com>
2023-03-07 14:04:09 -0300

    feat: Get finances

--- src/routes/finances.js
+++ src/routes/finances.js
@@ -34,7 +34,7 @@ router.post("/", async (req, res) => {
       });
     }
     //validação data
-    if (!date || title.length < 3) {
+    if (!date || date.length < 3) {
       return res.status(400).json({
         error: "Date is mandatory and must be yyyy-mm-dd",
       });
@@ -96,7 +96,7 @@ router.delete("/:id", async (req, res) => {
 
     const userQuery = await db.query(findEmail.findUser(email));
     if (!userQuery.rows[0]) {
-      return res.status(404).json({ error: "User already exist" });
+      return res.status(404).json({ error: "User doesn't exist" });
     }
 
     //queries db
@@ -130,7 +130,55 @@ router.delete("/:id", async (req, res) => {
 
     return res.status(200).json(deleteResponse.rows[0]);
   } catch (error) {
-    res.status(500).json(console.log(error));
+    return res.status(500).json({ error });
+  }
+});
+
+//##GET. http do get não tem body. Eu não passo parametro, e sim query.
+
+router.get("/", async (req, res) => {
+  try {
+    //?
+    const { date } = req.query;
+    const { email } = req.headers;
+
+    //validação data
+    if (!date || date.length != 10) {
+      return res.status(400).json({
+        error: "Date is mandatory and must be yyyy-mm-dd",
+      });
+    }
+
+    //validação de email
+    if (email.length < 5 || !email.includes("@")) {
+      return res.status(400).json({
+        error: "invalid email",
+      });
+    }
+    // validação do usuário por email
+
+    const userQuery = await db.query(findEmail.findUser(email));
+    if (!userQuery.rows[0]) {
+      return res.status(404).json({ error: "User doesn't exist" });
+    }
+
+    //## funcionou fazer um obj
+    const dateObj = new Date(date);
+    const year = dateObj.getFullYear();
+    const month = dateObj.getMonth();
+    const initDate = new Date(year, month, 1).toISOString();
+    const lastDate = new Date(year, month + 1, 0).toISOString(); // gambiarra JS. Como não tem dia 0, pega o ultimo do mês anterior
+
+    const text =
+      "SELECT fin.title, fin.value, fin.date, fin.user_id, fin.category_id, cat.name FROM finances as fin JOIN categories as cat ON fin.category_id = cat.id WHERE fin.user_id = $1 AND fin.date BETWEEN $2 AND $3 ORDER BY fin.date ASC";
+
+    const values = [userQuery.rows[0].id, initDate, lastDate];
+    const financeQuery = await db.query(text, values);
+
+    //Ainda posso não ter nenhum dado
+    return res.status(200).json(financeQuery.rows);
+  } catch (error) {
+    return res.status(500).json(console.log(error));
   }
 });
 
